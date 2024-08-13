6761b3d93831f4a23e8e821fcbda8d7cf44d8f2c
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-08-13 23:08:14 +0200

    Started adding Generative AI

---
+++ Scripts/ai_manager.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var http_request = $HTTPRequest 
+var history = []
+
+func _ready():
+	history.append("Let's play a roleplay game. You are now Dieter, a guy who is in prison because he stole pans from an old grandmother. You'll now have a conversation with another inmate.")
+	continue_chat()
+
+func continue_chat():
+	var url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" + load_api_key()
+	var header = ["Content-Type: application/json"]
+	var data = { "contents": []}
+	
+	for part in history:
+		data["contents"].append({ "parts": part })
+	
+	var json = JSON.stringify(data)
+	print(json)
+	
+	var error = http_request.request(url, header, HTTPClient.METHOD_POST, json)
+	if error != OK:
+		print("Error occured: " + error)
+
+func load_api_key():
+	var config = ConfigFile.new()
+	var err = config.load("res://config.cfg")
+	if err != OK:
+		print("Failed to load config file")
+		return ""
+	return config.get_value("API", "api_key", "")
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+	print(body.get_string_from_utf8())
+	
+	
