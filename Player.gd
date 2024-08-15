48c855b0ad03eb5b17a8229eb3aec460299839fb
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-08-15 13:18:12 +0200

    Added NPC

--- Player.gd
+++ Player.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
 var speed = 150
+var can_speak = false
+var is_speaking = false
+var can_loot = false
 
 func _physics_process(delta):
 	process_input(delta)
 
 func process_input(delta):
+	if is_speaking:
+		return
 	var is_walking = false
 	var input_vector = Vector2.ZERO
 	velocity.x = (Input.get_action_strength("right") - Input.get_action_strength("left")) * speed
@@ -21,3 +26,21 @@ func process_input(delta):
 		$"Idle-sheet".flip_h = needs_flip
 		$"Run-sheet".flip_h = needs_flip
 		$AnimationPlayer.play("walk")
+
+func _input(event):
+	if event.is_action("interact") && can_speak:
+		if is_speaking:
+			is_speaking = false
+		else: 
+			is_speaking = true
+
+
+func _on_area_2d_body_entered(body):
+	print(body)
+	if body.name == "Wizard":
+		can_speak = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Wizard":
+		can_speak = false
