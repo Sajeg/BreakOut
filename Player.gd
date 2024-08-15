58cbc8bcef7ccbc96c8087e8dea67dba9c2f18f2
Sajeg <84874195+Sajeg@users.noreply.github.com>
2024-08-15 12:21:34 +0200

    improved play movement

--- Player.gd
+++ Player.gd
@@ -1,20 +1,23 @@
-extends Node2D
+extends CharacterBody2D
 
-var speed = 5
+var speed = 150
 
-func _process(delta):
-	process_input()
+func _physics_process(delta):
+	process_input(delta)
 
-func process_input():
-	if Input.is_action_pressed("down"):
-		position.y += speed
-		$AnimationPlayer.play("walk")
-	if Input.is_action_pressed("up"):
-		position.y -= speed
-		$AnimationPlayer.play("walk")
-	if Input.is_action_pressed("right"):
-		position.x += speed
-		$AnimationPlayer.play("walk")
-	if Input.is_action_pressed("left"):
-		position.x -= speed
+func process_input(delta):
+	var is_walking = false
+	var input_vector = Vector2.ZERO
+	velocity.x = (Input.get_action_strength("right") - Input.get_action_strength("left")) * speed
+	velocity.y = (Input.get_action_strength("down") - Input.get_action_strength("up")) * speed
+	var is_idling = is_zero_approx(velocity.y) && is_zero_approx(velocity.x)
+	var needs_flip = velocity.x < 0
+	
+	move_and_slide()
+	
+	if (is_idling):
+		$AnimationPlayer.play("idle")
+	else:
+		$"Idle-sheet".flip_h = needs_flip
+		$"Run-sheet".flip_h = needs_flip
 		$AnimationPlayer.play("walk")
